a3cb37be12a6d7cd289635f79d8927959bb2f2c1
KyawMinHtway066813 <batman066813@gmail.com>
2024-03-05 23:13:14 +0630

    Nabar first-day

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,6 +1,16 @@
+import Link from "next/link";
+
 const Navbar = () => {
     return (
-        <div className="">Navbar</div>
+        <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+            {/* LOGO */}
+            <div className="">
+                <Link href="/" className="text-sm bg-black rounded-md p-1 font-semibold">
+                    <span className="text-white mr-1">KyawMin</span>
+                    <span className="w-12 h-8 rounded bg-white text-black">Htway</span>
+                </Link>
+            </div>
+        </div>
     )
 }
 
