29def7eebffeec0b20b06f17c5e33b88d7110aea
KyawMinHtway066813 <batman066813@gmail.com>
2024-03-08 20:00:48 +0630

    fix some error

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,4 +1,8 @@
+'use client'
+
 import Link from "next/link";
+import { useState } from "react";
+
 
 const links = [
     { url: "/", title: "Home"},
@@ -7,9 +11,18 @@ const links = [
     { url: "/contact", title: "Contact"}
 ];
 
+
 const Navbar = () => {
+    
+    const [open, setOpen] = useState(false);
+    
     return (
         <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+            <div className="hidden md:flex gap-4">
+                {links.map(link => (
+                    <Link href={link.url} key={link.title}>{link.title}</Link>
+                ))}
+            </div>
             {/* LOGO */}
             <div className="">
                 <Link href="/" className="text-sm bg-black rounded-md p-1 font-semibold">
@@ -18,19 +31,25 @@ const Navbar = () => {
                 </Link>
             </div>
             {/* RESPONSIVE MENU */}
-            <div className="">
+            <div className="md:hidden">
                 {/* MENU BUTTON */}
-                <button className="w-10 h-8 flex flex-col justify-between">
-                    <div className="w-10 h-1 bg-black rounded"></div>
-                    <div className="w-10 h-1 bg-black rounded"></div>
-                    <div className="w-10 h-1 bg-black rounded"></div>
+                <button className="w-10 h-8 flex flex-col justify-between z-50 relative" 
+                        onClick={() => setOpen((prev) => !prev)}
+                >
+                    <div className="w-10 h-1 bg-white rounded"></div>
+                    <div className="w-10 h-1 bg-white rounded"></div>
+                    <div className="w-10 h-1 bg-white rounded"></div>
                 </button>
                 {/* MENU LIST */}
-                <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+                {open && (
+                    <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
                     {links.map(link => (
-                        <Link href={link.url} key={link.title}>{link.title}</Link>
+                        <Link href={link.url} key={link.title}>
+                            {link.title}
+                        </Link>
                     ))}
                 </div>
+                )}
             </div>
         </div>
     )
