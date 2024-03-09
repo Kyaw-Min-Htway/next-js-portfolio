a9dc32eabf3616372cd71603795a3e36cf621258
KyawMinHtway066813 <batman066813@gmail.com>
2024-03-09 23:07:56 +0630

    animation insert

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { useState } from "react";
 import NavLink from "./navLink";
 import Image from "next/image";
+import {motion} from "framer-motion";
 
 
 const links = [
@@ -17,6 +18,33 @@ const links = [
 const Navbar = () => {
     
     const [open, setOpen] = useState(false);
+
+    const topVariants={
+        closed:{
+            rotate:0,
+        },
+        opened:{
+            rotate:45,
+            backgroundColor:"rgb(255,255,255)"
+        }
+    }
+    const centerVariants={
+        closed:{
+            opacity:1,
+        },
+        opened:{
+            opacity:0,
+        }
+    }
+    const bottomVariants={
+        closed: {
+            rotate: 0,
+        },
+        opened: {
+            rotate: -45,
+            backgroundColor: "rgb(255,255,255)"
+        }
+    }
     
     return (
         <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
@@ -60,9 +88,9 @@ const Navbar = () => {
                 <button className="w-10 h-8 flex flex-col justify-between z-50 relative" 
                         onClick={() => setOpen((prev) => !prev)}
                 >
-                    <div className="w-10 h-1 bg-white rounded"></div>
-                    <div className="w-10 h-1 bg-white rounded"></div>
-                    <div className="w-10 h-1 bg-white rounded"></div>
+                    <motion.div variants={topVariants} animate={open ? "opened" : "closed"} className="w-10 h-1 bg-black rounded origin-left"></motion.div>
+                    <motion.div variants={centerVariants} animate={open ? "opened" : "closed"} className="w-10 h-1 bg-black rounded"></motion.div>
+                    <motion.div variants={bottomVariants} animate={open ? "opened" : "closed"} className="w-10 h-1 bg-black rounded origin-left"></motion.div>
                 </button>
                 {/* MENU LIST */}
                 {open && (
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ const Homepage = () => {
     <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
       {/* IMAGE CONTAINER */}
       <div className="h-1/2 lg:h-full lg:w-1/2 relative">
-        <Image src="/hero.png" alt="" fill className="object-contain"/>
+        <Image src="/2.png" alt="" fill className="object-contain"/>
       </div>
       {/* TEXT CONTAINER */}
       <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
@@ -17,7 +17,7 @@ const Homepage = () => {
           my portfolio showcases a diverse collection of projects that reflect my commitment to excellence.
         </p>
         {/* BUTTONS */}
-        <div className="flex gap-4">
+        <div className="w-full flex gap-4">
           <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">View My Work</button>
           <button className="p-4 rounded-lg ring-1 ring-black">Contact Me</button>
         </div>
