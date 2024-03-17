f6f1469479536217645bd9dac9323693e77022e4
KyawMinHtway066813 <batman066813@gmail.com>
2024-03-17 22:37:37 +0630

    fill the about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -10,7 +10,7 @@ const AboutPage = () => {
         transition={{duration:1}}
       >
         {/* CONTAINER */}
-        <div className="">
+        <div className="h-full overflow-scroll lg:flex">
           {/* TEXT CONTAINER  */}
           <div className="p-4 sm:p-8 md:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64">
             {/* BIOGRAPHY CONTAINER */}
@@ -55,11 +55,117 @@ const AboutPage = () => {
             <div className="flex flex-col gap-12 justify-center pb-48">
               {/* EXPERIENCE TITLE */}
               <h1 className="font-bold text-2xl">EXPERIENCE</h1>
+              {/* EXPERIENCE LIST */}
+              <div className="">
+              {/* EXPERIENCE LIST ITEM */}
+              <div className="flex justify-between h-48">
+                {/* LEFT */}
+                <div className="w-1/3">
+                  {/* JOB TITLE */}
+                  <div className="bg-white p-3 font-semiblod rounded-b-lg rounded-s-lg">
+                    Junior Javascript Engineer
+                  </div>
+                  {/* JOB DESC */}
+                  <div className="p-3 text-sm italic">
+                    My current employment. Way better than the position before!
+                  </div>
+                  {/* JOB DATE */}
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2024 - Present
+                  </div>
+                </div>
+                {/* CENTER */}
+                <div className="w-1/6">
+                  {/* LINE  */}
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    {/* LINE CIRCCLE */}
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                {/* RIGHT */}
+                <div className="w-1/3">
+                <div className="bg-white p-3 font-semiblod rounded-b-lg rounded-s-lg">
+                    Junior Javascript Engineer
+                  </div>
+                  {/* JOB DESC */}
+                  <div className="p-3 text-sm italic">
+                    My current employment. Way better than the position before!
+                  </div>
+                  {/* JOB DATE */}
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2024 - Present
+                  </div>
+                </div>
+              </div>
               {/* EXPERIENCE LIST ITEM */}
               <div className="">
                 {/* LEFT */}
+                <div className="">
+                  {/* JOB TITLE */}
+                  <div className="">
+                    Junior Javascript Engineer
+                  </div>
+                  {/* JOB DESC */}
+                  <div className="">
+                    My current employment. Way better than the position before!
+                  </div>
+                  {/* JOB DATE */}
+                  <div className="">
+                    2024 - Present
+                  </div>
+                </div>
                 {/* CENTER */}
+                <div className="">
+                  {/* LINE  */}
+                  <div className="">
+                    {/* LINE CIRCCLE */}
+                    <div className=""></div>
+                  </div>
+                </div>
                 {/* RIGHT */}
+                <div className=""></div>
+              </div>
+              {/* EXPERIENCE LIST ITEM */}
+              <div className="">
+                {/* LEFT */}
+                <div className="">
+                  {/* JOB TITLE */}
+                  <div className="">
+                    Junior Javascript Engineer
+                  </div>
+                  {/* JOB DESC */}
+                  <div className="">
+                    My current employment. Way better than the position before!
+                  </div>
+                  {/* JOB DATE */}
+                  <div className="">
+                    2024 - Present
+                  </div>
+                </div>
+                {/* CENTER */}
+                <div className="">
+                  {/* LINE  */}
+                  <div className="">
+                    {/* LINE CIRCCLE */}
+                    <div className=""></div>
+                  </div>
+                </div>
+                {/* RIGHT */}
+                <div className="">
+                  {/* JOB TITLE */}
+                  <div className="bg-white p-3 font-semiblod rounded-b-lg rounded-s-lg">
+                    Junior Javascript Engineer
+                  </div>
+                  {/* JOB DESC */}
+                  <div className="p-3 text-sm italic">
+                    My current employment. Way better than the position before!
+                  </div>
+                  {/* JOB DATE */}
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2024 - Present
+                  </div>
+                </div>
+              </div>
               </div>
             </div>
           </div>
