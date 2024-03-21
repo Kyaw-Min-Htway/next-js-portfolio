b236cacd177144ab8d713ebe4d492d3e56a14e1a
KyawMinHtway066813 <batman066813@gmail.com>
2024-03-21 22:53:47 +0630

    portfolio page adding

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,15 +1,24 @@
 'use client';
 
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+
 
 const PortfolioPage = () => {
+
+  const ref = useRef()
+  const {scrollYProgress} = useScroll({target: ref});
+  const x = useTransform(scrollYProgress, [0, 1], ["0%","100%"]);
     return (
         <motion.div className="h-full" 
         initial={{y:"-220vh"}} 
         animate={{y:"0%"}} 
         transition={{duration:1}}
       >
-        Portfolio
+        <div className="h-[600vh] relative" ref={ref}>
+          <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">My works</div>
+          <div className="sticky top-0 flex h-screen gap-4 items-center"></div>
+        </div>
       </motion.div>
     )
 }
