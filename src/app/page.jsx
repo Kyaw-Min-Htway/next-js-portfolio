8d84ebde5b606da840ae4741b8a145d18b66ce3f
KyawMinHtway066813 <batman066813@gmail.com>
2024-07-13 20:41:03 +0630

    2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -26,25 +26,27 @@ const Homepage = () => {
     >
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 space-y-8 lg:space-y-0 lg:space-x-8">
         {/* IMAGE CONTAINER */}
-        <div className="h-64 lg:h-auto lg:w-1/2 relative">
+        <div className="relative w-full lg:w-1/2 h-64 lg:h-auto">
           <Image src="/2.png" alt="Developer" fill className="object-contain" />
         </div>
         {/* TEXT CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center text-center lg:text-left">
           {/* TITLE */}
-          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">Junior Full Stack Web Developer</h1>
+          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-gray-800">Junior Full Stack Web Developer</h1>
           {/* DESCRIPTION */}
-          <p className="text-base md:text-lg lg:text-xl">Aspiring Junior Web Developer seeking to leverage strong programming skills and practical experience with modern web technologies in a fast-paced and innovative company. Committed to continuous learning and enthusiastic about contributing to meaningful web development projects.</p>
+          <p className="text-base md:text-lg lg:text-xl text-gray-600">Aspiring Junior Web Developer seeking to leverage strong programming skills and practical experience with modern web technologies in a fast-paced and innovative company. Committed to continuous learning and enthusiastic about contributing to meaningful web development projects.</p>
           {/* BUTTONS */}
           <div className="w-full flex flex-col sm:flex-row gap-4">
             <button 
               onClick={() => { downloadFileAtURl(CV) }} 
-              className="p-4 rounded-lg ring-1 ring-black bg-black text-white transition duration-300 ease-in-out transform hover:scale-105"
+              className="p-4 rounded-lg ring-1 ring-black bg-black text-white transition duration-300 ease-in-out transform hover:scale-105 hover:bg-gray-700"
             >
               Download CV
             </button>
             <Link href="/contact">
-              <button className="p-4 rounded-lg ring-1 ring-black transition duration-300 ease-in-out transform hover:scale-105">
+              <button 
+                className="p-4 rounded-lg ring-1 ring-black transition duration-300 ease-in-out transform hover:scale-105 hover:bg-gray-200"
+              >
                 Contact Me
               </button>
             </Link>
